6cba8da0fd59ee60e82d23265107d5ca5d5a0655
schaumb <schaumb@gmail.com>
2016-10-20 14:08:10 +0200

    11. task

---
+++ quiz/Task11.java
@@ -0,0 +1,14 @@
+public class Task11 {
+
+    public static void main(String[] args) {
+        String str = new String("foo");
+
+        System.out.println("A: " + (str == "foo"));
+        System.out.println("B: nem. Hatékonyabb a String str = \"foo\"");
+        System.out.println("C: nem. Az értékadásban csak 1 objektumot hoz létre. \n" +
+            "  A string poolban létezhet már, de azt nem biztos hogy itt keletkezik. \n" + 
+            "  --- Esetleg a konstruktor egy másik (típusú) objektumot létrehozhat \n" + 
+            "  --- ami miatt a számosságuk 2 lesz?");
+        System.out.println("D: Az elozo magyarazat miatt igen, ez az egyetlen valid valasz");
+    }
+}
