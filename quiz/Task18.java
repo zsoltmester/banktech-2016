a9e36c7e967c35b66d13201063b1f99345e1efae
schaumb <schaumb@gmail.com>
2016-10-21 08:02:15 +0200

    24. task wut - Task18.java threadstart

--- quiz/Task18.java
+++ quiz/Task18.java
@@ -3,6 +3,6 @@ public class Task18 {
     public static void main(String[] args) {
         new Thread(() -> {
             new EgyOsztaly(); // jól értem ezt?
-        });
+        }).start();
     }
 }
