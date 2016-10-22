e9dc95489c702ed2e43a37126d8f2bbe8c2d41d2
zsmester <zsolt.mester67@gmail.com>
2016-10-22 22:13:00 +0200

    8. task update

--- quiz/MyObjectMain.java
+++ quiz/MyObjectMain.java
@@ -13,5 +13,7 @@ public class MyObjectMain {
 
 	public static void main(String[] args) {
 		Object obj = new MyObject(){};
+		System.out.println(obj.getClass().getTypeName()); // class, interface: MyObjectMain$1
+		System.out.println(obj.getClass().getSuperclass().getTypeName()); // class: MyObjectMain$MyObject, interface: java.lang.Object
 	}
 }
