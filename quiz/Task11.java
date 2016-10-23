2388273bcf4270c2db0ee37a4e4dfa8663050454
zsmester <zsolt.mester67@gmail.com>
2016-10-23 21:21:30 +0200

    11; Add a comment

--- quiz/Task11.java
+++ quiz/Task11.java
@@ -6,9 +6,9 @@ public class Task11 {
         System.out.println("A: " + (str == "foo"));
         System.out.println("B: nem. Hatékonyabb a String str = \"foo\"");
         System.out.println("C: nem. Az értékadásban csak 1 objektumot hoz létre. \n" +
-            "  A string poolban létezhet már, de azt nem biztos hogy itt keletkezik. \n" + 
-            "  --- Esetleg a konstruktor egy másik (típusú) objektumot létrehozhat \n" + 
-            "  --- ami miatt a számosságuk 2 lesz?");
+            "  A string poolban létezhet már, de azt nem biztos hogy itt keletkezik. \n" +
+            "  --- Esetleg a konstruktor egy másik (típusú) objektumot létrehozhat \n" +
+            "  --- ami miatt a számosságuk 2 lesz?"); // zsolt: szerintem ez implementáció (openjdk vs oracle jdk) és java verzió függű is lehet, úgyhogy nem hiszem
         System.out.println("D: Az elozo magyarazat miatt igen, ez az egyetlen valid valasz");
     }
 }
