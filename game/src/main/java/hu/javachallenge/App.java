2184720edcc50b2453248e09a881d25e3ea037f0
zsmester <zsolt.mester67@gmail.com>
2016-10-30 11:56:46 +0100

    Add OkHttp dependency and add dependencies to the jar.

--- game/src/main/java/hu/javachallenge/App.java
+++ game/src/main/java/hu/javachallenge/App.java
@@ -1,12 +1,24 @@
 package hu.javachallenge;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
 /**
  * Hello world!
  */
-public class App
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .addHeader("TEAMTOKEN", "3120A9456D1FD8706C223980245717B7")
+                .url("http://195.228.45.100:8080/jc16-srv/game")
+                .build();
+
+        Response response = client.newCall(request).execute();
+        System.out.println(response.body().string());
     }
 }
