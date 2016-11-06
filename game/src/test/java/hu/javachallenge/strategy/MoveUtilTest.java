cbec9147663f31bbef18a573b7ab1843e0e1a381
zsmester <zsolt.mester67@gmail.com>
2016-11-06 22:57:53 +0100

    Just added some test.

--- game/src/test/java/hu/javachallenge/strategy/MoveUtilTest.java
+++ game/src/test/java/hu/javachallenge/strategy/MoveUtilTest.java
@@ -6,6 +6,7 @@ import hu.javachallenge.bean.Position;
 import hu.javachallenge.bean.Submarine;
 import hu.javachallenge.map.IMap;
 import junit.framework.TestCase;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -19,7 +20,7 @@ public class MoveUtilTest {
     private Submarine submarine;
     private MapConfiguration mockedMapConfiguration;
 
-    @org.junit.Before
+    @Before
     public void setUp() throws NoSuchFieldException, IllegalAccessException {
 
         IMap mockedMap = Mockito.mock(IMap.class);
@@ -36,12 +37,23 @@ public class MoveUtilTest {
 
     @Test
     public void getAngleForTargetPosition() throws Exception {
-        submarine.setPosition(new Position(0, -10.0));
+        submarine.setPosition(new Position(-123, 0));
+        TestCase.assertEquals(0, MoveUtil.getAngleForTargetPosition(submarine, new Position(0, 0)), 0.00000001);
 
+        submarine.setPosition(new Position(-5, -5));
+        TestCase.assertEquals(45, MoveUtil.getAngleForTargetPosition(submarine, new Position(0, 0)), 0.00000001);
+
+        submarine.setPosition(new Position(0, -5));
         TestCase.assertEquals(90, MoveUtil.getAngleForTargetPosition(submarine, new Position(0, 0)), 0.00000001);
+
+        submarine.setPosition(new Position(5, -5));
+        TestCase.assertEquals(90, MoveUtil.getAngleForTargetPosition(submarine, new Position(5, 0)), 0.00000001);
+
+        submarine.setPosition(new Position(-5, -5));
+        TestCase.assertEquals(45, MoveUtil.getAngleForTargetPosition(submarine, new Position(0, 0)), 0.00000001);
     }
 
-    @org.junit.Test
+    @Test
     public void getTurnForTargetPositionTestAngle() throws Exception {
         submarine.setPosition(new Position(2, 2));
         submarine.setAngle(0.0);
@@ -56,7 +68,7 @@ public class MoveUtilTest {
         TestCase.assertEquals(-45.0, MoveUtil.getTurnForTargetPosition(submarine, new Position(3, 1)), 0.00000001);
     }
 
-    @org.junit.Test
+    @Test
     public void getTurnForTargetPositionTestMaxSteering() throws Exception {
         submarine.setPosition(new Position(10, 10));
         submarine.setAngle(36.0);
@@ -96,10 +108,14 @@ public class MoveUtilTest {
         submarine.setPosition(new Position(0, 50));
         submarine.setVelocity(20.0);
 
-        TestCase.assertEquals(-5.0, MoveUtil.getAccelerationToCloseThere(submarine, new Position(0, 23)),0.00000001);
-        TestCase.assertEquals(-5.0, MoveUtil.getAccelerationToCloseThere(submarine, new Position(0, 20)),0.00000001);
+        TestCase.assertEquals(-mockedMapConfiguration.getMaxAccelerationPerRound(), MoveUtil.getAccelerationToCloseThere(submarine, new Position(0, 23)), 0.00000001);
+        TestCase.assertEquals(-mockedMapConfiguration.getMaxAccelerationPerRound(), MoveUtil.getAccelerationToCloseThere(submarine, new Position(0, 20)), 0.00000001);
 
-        TestCase.assertEquals(0.0, MoveUtil.getAccelerationToCloseThere(submarine, new Position(0, 0)),0.00000001);
+        TestCase.assertEquals(0.0, MoveUtil.getAccelerationToCloseThere(submarine, new Position(0, 0)), 0.00000001);
+
+        submarine.setPosition(new Position(0, 1));
+        submarine.setVelocity(0.0);
+        TestCase.assertEquals(0.0, MoveUtil.getAccelerationToCloseThere(submarine, new Position(0, 0)), 0.00000001);
 
         // test of speed up, and slow down to the closest of the destination
         submarine.setVelocity(0.0);
@@ -107,12 +123,12 @@ public class MoveUtilTest {
         Position to = new Position(0, -212.1005428);
 
         boolean reachedPoint = false;
-        for(int i = 0; i < 15; ++i) {
+        for (int i = 0; i < 15; ++i) {
             double acceleration = MoveUtil.getAccelerationToCloseThere(submarine, to);
             submarine.setVelocity(submarine.getVelocity() + acceleration);
             submarine.getPosition().setY(submarine.getPosition().getY() - submarine.getVelocity());
 
-            if(submarine.getPosition().distance(to) < 2.5 && submarine.getVelocity() == 0.0) {
+            if (submarine.getPosition().distance(to) < (mockedMapConfiguration.getMaxAccelerationPerRound() / 2f) && submarine.getVelocity() == 0.0) {
                 reachedPoint = true;
                 break;
             }
