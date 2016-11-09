6417167e6a6d50ea8986cf36ca0ba9d9d5e3d7dd
schaumb <schaumb@gmail.com>
2016-11-09 06:26:14 +0100

    moar offlineCommunication AKA more server code

--- game/src/main/java/hu/javachallenge/communication/OfflineCommunicatorImpl.java
+++ game/src/main/java/hu/javachallenge/communication/OfflineCommunicatorImpl.java
@@ -1,9 +1,13 @@
 package hu.javachallenge.communication;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import hu.javachallenge.bean.*;
 import hu.javachallenge.map.IMap;
 import hu.javachallenge.processor.Processor;
+import hu.javachallenge.strategy.ScoutStrategy;
+import hu.javachallenge.strategy.moving.IChangeMovableObject;
 
+import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -13,56 +17,180 @@ import java.util.stream.Collectors;
 public class OfflineCommunicatorImpl implements Communicator {
     static class ServerGame implements Runnable {
         private Game game = new Game();
-        private List<Submarine> submarineList = new ArrayList<>();
         private List<Entity> entityList = new ArrayList<>();
         private HashMap<Long, MoveRequest> moves = new HashMap<>();
         private HashMap<Long, ShootRequest> shoots = new HashMap<>();
         private HashSet<Long> extSonar = new HashSet<>();
         private Thread tickerThread;
+        private Long maxId = 0L;
 
         public synchronized Game getGame() {
             return game;
         }
 
         public synchronized List<Submarine> getSubmarineList() {
-            return submarineList;
+            return entityList.stream()
+                    .filter(e -> e.getOwner().getName().equals(IMap.OUR_NAME))
+                    .filter(e -> (e instanceof Submarine))
+                    .map(e -> (Submarine) e)
+                    .collect(Collectors.toList());
         }
 
         public synchronized void join() {
             game.setStatus(Processor.GAME_STATUS.WAITING.name());
-            game.setScores(new Scores());
-            game.setMapConfiguration(new MapConfiguration());
-            game.setConnectionStatus(new ConnectionStatus());
             game.setRound(0);
 
             // mapConf
-            MapConfiguration config = game.getMapConfiguration();
-            
+            try {
+                game.setMapConfiguration(new ObjectMapper().readValue("{TODO}", MapConfiguration.class));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
             // conn status + scores
             HashMap<String, Boolean> map = new HashMap<>();
             map.put(IMap.OUR_NAME, true);
             map.put("BOT", true);
+
+            game.setConnectionStatus(new ConnectionStatus());
             game.getConnectionStatus().setConnected(map);
 
             HashMap<String, Integer> scores = new HashMap<>();
             scores.put(IMap.OUR_NAME, 0);
             scores.put("BOT", 0);
+
+            game.setScores(new Scores());
             game.getScores().setScores(scores);
 
-            // create entities
+            // TODO create entities
+
 
             tickerThread = new Thread(this);
             tickerThread.start();
             // & start the game
         }
 
-        private synchronized Submarine find(Long id) {
-            return submarineList.stream().filter(s -> s.getId().equals(id)).findFirst().orElse(null);
+        public boolean isValidPosition(Position position) {
+            return position != null &&
+                    0 <= position.getX() && position.getX() < game.getMapConfiguration().getWidth() &&
+                    0 <= position.getY() && position.getY() < game.getMapConfiguration().getHeight();
+        }
+
+        private class MyStrategy extends ScoutStrategy {
+            public MyStrategy(Long submarineId, Position... targets) {
+                super(submarineId, targets);
+            }
+
+            @Override
+            public Submarine getSubmarine() {
+                return entityList.stream()
+                        .filter(e -> e.getId().equals(submarineId))
+                        .map(e -> (Submarine) e)
+                        .findFirst().orElse(null);
+            }
+
+            @Override
+            protected void changeTargetIfNeed() {
+                // TODO
+                getTargets().add(new Position(2, 2));
+
+                super.changeTargetIfNeed();
+            }
+        }
+
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(200);
+                for(int i = 1; i < game.getMapConfiguration().getRounds() + 1; ++i) {
+                    synchronized (this) {
+                        game.setStatus(Processor.GAME_STATUS.RUNNING.name());
+                        game.setRound(i);
+                    }
+                    Thread.sleep(1000);
+                    synchronized (this) {
+                        for(Submarine submarine : getSubmarineList()) {
+                            MoveRequest moveRequest = moves.get(submarine.getId());
+                            ShootRequest shootRequest = shoots.get(submarine.getId());
+                            Boolean extendSonar = extSonar.contains(submarine.getId());
+
+                            // extSonar
+                            if (extendSonar) {
+                                submarine.setSonarCooldown(game.getMapConfiguration().getExtendedSonarCooldown());
+                                submarine.setSonarExtended(game.getMapConfiguration().getExtendedSonarRounds());
+                            } else {
+                                if(submarine.getSonarExtended() > 0) {
+                                    submarine.setSonarExtended(submarine.getSonarExtended() - 1);
+                                }
+                                if(submarine.getSonarCooldown() > 0) {
+                                    submarine.setSonarCooldown(submarine.getSonarCooldown() - 1);
+                                }
+                            }
+
+                            // shoot
+                            if (shootRequest != null) {
+                                Entity entity = new Entity();
+                                entity.setId(++maxId);
+                                entity.setPosition(submarine.getPosition());
+                                entity.setType(Entity.TORPEDO);
+                                entity.setVelocity(game.getMapConfiguration().getTorpedoSpeed());
+                                entity.setAngle(shootRequest.getAngle());
+                                entity.setRoundsMoved(1);
+                                entity.setOwner(new Owner(IMap.OUR_NAME));
+
+                                IChangeMovableObject.ZERO_MOVE.moveToNext(entity);
+                            }
+
+                            // move
+                            IChangeMovableObject<MovableObject> move = IChangeMovableObject.ZERO_MOVE;
+                            if(moveRequest != null) {
+                                move = new IChangeMovableObject.FixMove<>(moveRequest.getSpeed(), moveRequest.getTurn());
+                            }
+                            move.moveToNext(submarine);
+                        }
+
+
+                        // + moving others
+                        entityList = entityList.stream()
+                                .filter(e -> {
+                                    if(e.getType().equals(Entity.SUBMARINE) &&
+                                            !e.getOwner().getName().equals(IMap.OUR_NAME)) {
+                                        new MyStrategy(e.getId()).moveToNext((Submarine) e);
+                                    } else if (e.getType().equals(Entity.TORPEDO)) {
+                                        IChangeMovableObject.ZERO_MOVE.moveToNext(e);
+                                        if(e.getRoundsMoved() >= game.getMapConfiguration().getTorpedoRange()) {
+                                            return false;
+                                        }
+                                        e.setRoundsMoved(e.getRoundsMoved());
+                                    }
+                                    return isValidPosition(e.getPosition());
+                                })
+                                .filter(e -> true) // TODO nem-e ment neki torpedónak
+                                .filter(e -> true) // TODO nem-e ment neki szigetnek
+                                .collect(Collectors.toList());
+
+                        // remove messages
+                        moves.clear();
+                        shoots.clear();
+                        extSonar.clear();
+                    }
+                }
+                synchronized (this) {
+                    game.setStatus(Processor.GAME_STATUS.ENDED.name());
+                    game.setRound(game.getMapConfiguration().getRounds());
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            tickerThread = null;
+        }
+
+        private synchronized Submarine findPlayerSubmarine(Long id) {
+            return getSubmarineList().stream().filter(s -> s.getId().equals(id)).findFirst().orElse(null);
         }
 
         public synchronized MoveResponse moveSubmarine(Long submarineId, MoveRequest request) {
-            Submarine submarine = find(submarineId);
+            Submarine submarine = findPlayerSubmarine(submarineId);
 
             if(submarine == null) {
                 MoveResponse response = new MoveResponse();
@@ -86,7 +214,7 @@ public class OfflineCommunicatorImpl implements Communicator {
         }
 
         public synchronized ShootResponse shoot(Long submarineId, ShootRequest request) {
-            Submarine submarine = find(submarineId);
+            Submarine submarine = findPlayerSubmarine(submarineId);
 
             if(submarine == null) {
                 ShootResponse response = new ShootResponse();
@@ -109,7 +237,7 @@ public class OfflineCommunicatorImpl implements Communicator {
         }
 
         public synchronized SonarResponse sonar(Long submarineId) {
-            Submarine submarine = find(submarineId);
+            Submarine submarine = findPlayerSubmarine(submarineId);
 
             if(submarine == null) {
                 SonarResponse response = new SonarResponse();
@@ -132,7 +260,7 @@ public class OfflineCommunicatorImpl implements Communicator {
         }
 
         public synchronized ExtendSonarResponse extSonar(Long submarineId) {
-            Submarine submarine = find(submarineId);
+            Submarine submarine = findPlayerSubmarine(submarineId);
 
             if(submarine == null) {
                 ExtendSonarResponse response = new ExtendSonarResponse();
@@ -151,33 +279,6 @@ public class OfflineCommunicatorImpl implements Communicator {
             ExtendSonarResponse response = new ExtendSonarResponse();
             setStatus(response, 0);
             return response;
-        }
-
-        @Override
-        public void run() {
-            try {
-                Thread.sleep(200);
-                for(int i = 1; i < game.getMapConfiguration().getRounds() + 1; ++i) {
-                    synchronized (this) {
-                        game.setStatus(Processor.GAME_STATUS.RUNNING.name());
-                        game.setRound(i);
-                    }
-                    Thread.sleep(1000);
-                    synchronized (this) {
-
-                        // TODO commands
-
-                        // + moving others
-                    }
-                }
-                synchronized (this) {
-                    game.setStatus(Processor.GAME_STATUS.ENDED.name());
-                    game.setRound(game.getMapConfiguration().getRounds());
-                }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            tickerThread = null;
         }
     }
 
